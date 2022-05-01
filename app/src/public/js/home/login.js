9ad600cc7941245805b72e41baa2525be78c7e78
xgro <zero1401@naver.com>
2022-05-01 22:26:10 +0900

    로그인 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,9 +1,9 @@
 "use strict";
 
 
-const id = document.querySelector("#id"),
-    psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("button");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
 
 loginBtn.addEventListener("click", login);
 
@@ -13,6 +13,14 @@ function login() {
         psword : psword.value,
     }
     console.log(req);
+
+    fetch("/login", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req)
+    });
 }
 
 
